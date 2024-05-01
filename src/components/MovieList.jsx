573343e4a1d252bc3de25a648c9db8dd6df22fec
AValenzue <zuellagraphy@gmail.com>
2024-05-01 14:27:34 +0200

    implement delete button

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,44 @@
+import { useState } from "react";
 import movies from "../data/movies.json";
 function MovieList() {
   console.log(movies);
 
-  return <section className="MovieList">
-    {movies.map((movieObj,index,arr)=> {
-return(
-    <div className="card" key={movieObj.id}>
-        <p>Title: {movieObj.title}</p>
-        <p>Rating: {movieObj.rating}</p>
+  const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+  const deleteMovie = (movieId) => {
+    console.log(`deleting movie with the id... ${movieId}`);
 
-    </div>
-)
-    })}
-  </section>;
+    const newList = moviesToDisplay.filter((movie) => {movie.id !== movieId });
+
+        //   if (movie.id === movieId) {
+    //     return false;
+    //   } else {
+    //     return true;
+    //   }
+   
+    console.log(newList);
+
+    setMoviesToDisplay(newList);
+  };
+
+  return (
+    <section className="MovieList">
+        <h1>Number of movies: {moviesToDisplay.length}</h1>
+      {moviesToDisplay.map((movieObj) => {
+        return (
+          <div className="card" key={movieObj.id}>
+            <p>Title: {movieObj.title}</p>
+            <p>Rating: {movieObj.rating}</p>
+            <button
+              onClick={() => {
+                deleteMovie(movieObj.id);
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        );
+      })}
+    </section>
+  );
 }
 export default MovieList;
